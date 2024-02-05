481d2bfb7ba648ea8d7b24c4baf091bc365bfda0
Steven R. Baker <steven@stevenrbaker.com>
2024-02-05 23:03:39 +0100

    `bin/rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_220002) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_05_220304) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -55,6 +55,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_220002) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cards", force: :cascade do |t|
+    t.integer "card_column_id", null: false
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_column_id"], name: "index_cards_on_card_column_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cards", "card_columns"
 end
