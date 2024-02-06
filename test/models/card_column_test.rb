f20dd0e64993bde359f761dc977ff455cbcaecdf
Steven R. Baker <steven@stevenrbaker.com>
2024-02-06 15:50:12 +0100

    Back-fill the model tests.

--- test/models/card_column_test.rb
+++ test/models/card_column_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class CardColumnTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a title" do
+    card_column = CardColumn.new title: ''
+
+    assert card_column.invalid?
+    assert card_column.errors.has_key? :title
+    assert_equal ["can't be blank"], card_column.errors[:title]
+  end
 end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "belongs to card_column" do
+    card_column = card_columns(:one)
+
+    card = Card.new card_column: card_column
+
+    assert card.valid?
+  end
+
+  test "must belong to card_column" do
+    card = Card.new card_column_id: nil
+
+    assert card.invalid?
+    assert card.errors.has_key? :card_column
+    assert_equal ["must exist"], card.errors[:card_column]
+  end
 end
