071feb3ce313318d0b192ae03314ee13e1bfaaa9
Steven R. Baker <steven@stevenrbaker.com>
2024-03-01 18:55:05 +0100

    Useless comments.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,25 +1,20 @@
 class CardsController < ApplicationController
   before_action :set_card, only: %i[ show edit update destroy ]
 
-  # GET /cards or /cards.json
   def index
     @cards = Card.all
   end
 
-  # GET /cards/1 or /cards/1.json
   def show
   end
 
-  # GET /cards/new
   def new
     @card = Card.new
   end
 
-  # GET /cards/1/edit
   def edit
   end
 
-  # POST /cards or /cards.json
   def create
     @card = Card.new(card_params)
 
@@ -34,7 +29,6 @@ class CardsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cards/1 or /cards/1.json
   def update
     respond_to do |format|
       if @card.update(card_params)
@@ -47,7 +41,6 @@ class CardsController < ApplicationController
     end
   end
 
-  # DELETE /cards/1 or /cards/1.json
   def destroy
     @card.destroy!
 
@@ -58,12 +51,10 @@ class CardsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_card
       @card = Card.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def card_params
       params.require(:card).permit(:lane_id, :title, :body)
     end
