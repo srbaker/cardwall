cbc8d42d09ea371cf8594af23187601a7628c852
Steven R. Baker <steven@stevenrbaker.com>
2024-03-02 02:13:57 +0100

    Fix the migration to update the existing data with positions.

--- db/migrate/20240302010823_add_position_to_card_and_lane.rb
+++ db/migrate/20240302010823_add_position_to_card_and_lane.rb
@@ -2,5 +2,13 @@ class AddPositionToCardAndLane < ActiveRecord::Migration[7.1]
   def change
     add_column :cards, :position, :integer
     add_column :lanes, :position, :integer
+
+    Card.order(:updated_at).each.with_index(1) do |card, index|
+      card.update_column :position, index
+    end
+
+    Lane.order(:updated_at).each.with_index(1) do |lane, index|
+      lane.update_column :position, index
+    end
   end
 end
