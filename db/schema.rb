2b6fcc7a74b864af5d72f8f875e8c57470695d91
Steven R. Baker <steven@stevenrbaker.com>
2024-03-02 02:10:03 +0100

    `bin/rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_182102) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_010823) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_182102) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["lane_id"], name: "index_cards_on_lane_id"
   end
 
@@ -62,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_182102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "project_id"
+    t.integer "position"
     t.index ["project_id"], name: "index_lanes_on_project_id"
   end
 
