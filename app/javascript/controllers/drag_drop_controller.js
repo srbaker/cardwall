fca2d5ac9b472bc77a4c21d72dc9ffd871062ed5
Steven R. Baker <steven@stevenrbaker.com>
2024-03-12 14:32:53 +0100

    Move cards between lanes.

--- app/javascript/controllers/drag_drop_controller.js
+++ app/javascript/controllers/drag_drop_controller.js
@@ -4,10 +4,14 @@ import Sortable from "sortablejs"
 
 export default class extends Controller {
     connect() {
-        this.sortable = Sortable.create(this.element, {
-            animation: 150,
-            onEnd: this.end.bind(this)
-        })
+        let sortableParams = {
+            animation: 150, onEnd: this.end.bind(this)
+        }
+        if (this.element.dataset.group) {
+            sortableParams = {...sortableParams, group: this.element.dataset.group};
+        }
+
+        this.sortable = Sortable.create(this.element, sortableParams)
     }
 
     async end(event) {
@@ -16,7 +20,11 @@ export default class extends Controller {
         let resource = event.item.dataset.resource;
         const formData = new FormData();
         formData.append(`${resource}[position]`, event.newIndex + 1);
+        if (event.to.dataset.groupField) {
+            console.log(JSON.stringify(event.to.dataset))
+            formData.append(`${resource}[${event.to.dataset.groupField}]`, event.to.dataset.groupId)
+        }
 
-        await patch(url, {body: formData} );
+        await patch(url, {body: formData});
     }
 }
